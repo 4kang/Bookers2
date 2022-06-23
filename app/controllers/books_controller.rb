c4f5877de8c75f48aae6d5f4e09d21ef41dcca0d
4kang <106650699+4kang@users.noreply.github.com>
2022-06-23 06:53:02 +0000

    [Add]5-24

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @book = Book.find(params[:id])
@@ -28,6 +29,9 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to book_path(@book.id)
+    end
   end
 
   def update
@@ -50,6 +54,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body, :profile_image)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
   end
 
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,5 @@ class HomeController < ApplicationController
 
   end
 
-  def about
-
-  end
-
 end
 
